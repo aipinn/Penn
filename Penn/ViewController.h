fc1ca18ed593cf448e3c4e6713fc3d7046e246a7
pengpeng <15517201217@163.com>
2017-09-07 13:48:38 +0800

    The first commit

---
+++ Penn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Penn
+//
+//  Created by SanRong on 2017/9/7.
+//  Copyright © 2017年 SanRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Penn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Penn
+//
+//  Created by SanRong on 2017/9/7.
+//  Copyright © 2017年 SanRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
