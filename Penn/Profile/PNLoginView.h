0f51a2a507e16c30653236db59eb4084126e481e
pengpeng <15517201217@163.com>
2017-12-02 18:11:44 +0800

    add pop animation

---
+++ Penn/Profile/PNLoginView.h
@@ -0,0 +1,13 @@
+//
+//  PNLoginView.h
+//  Penn
+//
+//  Created by SanRong on 2017/12/2.
+//  Copyright © 2017年 SanRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PNLoginView : UIView
+
+@end
---
+++ Penn/Profile/PNLoginView.m
@@ -0,0 +1,21 @@
+//
+//  PNLoginView.m
+//  Penn
+//
+//  Created by SanRong on 2017/12/2.
+//  Copyright © 2017年 SanRong. All rights reserved.
+//
+
+#import "PNLoginView.h"
+
+@implementation PNLoginView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
