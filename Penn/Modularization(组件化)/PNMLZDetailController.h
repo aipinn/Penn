e21a0484b55e0cf77e80517d383a2d49be4b09ca
aipinn <allenpengplay@gmail.com>
2018-10-11 23:13:38 +0800

    Add modularization file

---
+++ Penn/Modularization(组件化)/PNMLZDetailController.h
@@ -0,0 +1,17 @@
+//
+//  PNMLZDetailController.h
+//  Penn
+//
+//  Created by pinn on 2018/10/11.
+//  Copyright © 2018 PENN. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PNMLZDetailController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Penn/Modularization(组件化)/PNRouter.m
@@ -0,0 +1,13 @@
+//
+//  PNRouter.m
+//  Penn
+//
+//  Created by pinn on 2018/10/11.
+//  Copyright © 2018 PENN. All rights reserved.
+//
+
+#import "PNRouter.h"
+
+@implementation PNRouter
+
+@end
